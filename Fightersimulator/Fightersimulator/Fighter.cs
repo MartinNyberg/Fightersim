2b1dd0c91b7b536068753bed542080c59213235c
MartinNyberg <55439200+MartinNyberg@users.noreply.github.com>
2019-10-08 13:15:00 +0200

    Start

---
+++ Fightersimulator/Fightersimulator/Fighter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fightersimulator
+{
+    class Fighter
+    {
+        private int hp = 1000;
+
+        public string name = "";
+
+        public void Attack()
+        {
+            Random generator = new Random();
+            int damage = generator.Next(250, 600);
+            
+
+        }
+
+        public void Hurt()
+        {
+
+        }
+
+
+    }
+}
